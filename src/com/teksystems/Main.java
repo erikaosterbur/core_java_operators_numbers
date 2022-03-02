3b0fe6e1616bc77e7ce93856cf78df024de27668
erikaosterbur <erikaosterbur@gmail.com>
2022-03-02 14:18:20 -0600

    Problem eight done

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -154,10 +154,19 @@ public class Main {
 
     public static void problemEight () {
         // Write a program that declares 2 integer variables, x, and y, and then assigns 5 to x and 8 to y.
+        int x = 5;
+        int y = 8;
+
         // Create another variable sum and assign the value of ++x added to y and print the result.
         // Notice the value of the sum (should be 14).
+        int sum = ++x + y;
+        System.out.println(sum);
+
         // Now change the increment operator to postfix (x++) and re-run the program.
         // Notice what the value of sum is.
+        sum = x++ + y;
+        System.out.println(sum);
+
         // The first configuration incremented x and then calculated the sum
         // while the second configuration calculated the sum and then incremented x.
 
